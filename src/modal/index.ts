d2748473a6fe45b51590a88ce9e79664751bd097
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-05-31 11:31:24 +0700

    init modal

--- src/modal/index.ts
+++ src/modal/index.ts
@@ -1,9 +1,25 @@
 import fmtUnit from '../_util/fmtUnit';
 
+type Props = {
+  className: string;
+  topImageSize: 'lg' | 'md' | 'sm';
+  showClose: boolean;
+  closeType: string;
+  mask: boolean;
+  buttonsLayout: 'horizontal' | 'vertical';
+  disableScroll: boolean;
+  maskClick: boolean;
+  buttons: string[];
+  onModalClick?: () => void;
+  onButtonClick?: (event: unknown) => void;
+  onModalClose?: () => void;
+  onMaskClick?: () => void;
+};
 Component({
   data: {
-    adviceClose: fmtUnit(26),
-    normalClose: fmtUnit(18),
+    _buttonsLayout: '',
+    adviceClose: fmtUnit('26'),
+    normalClose: fmtUnit('18'),
   },
   props: {
     className: '',
@@ -11,30 +27,48 @@ Component({
     showClose: false,
     closeType: '0',
     mask: true,
-    show: false,
+    buttonsLayout: 'horizontal',
+    disableScroll: true,
     maskClick: false,
+    buttons: [],
+    onModalClick: () => {},
+    onButtonClick: () => {},
+    onModalClose: () => {},
+    onMaskClick: () => {},
+  } as Props,
+  didMount() {
+    const { buttons, buttonsLayout } = this.props;
+    if (buttons && buttons.length > 2) {
+      this.setData({
+        _buttonsLayout: 'vertical',
+      });
+    } else {
+      this.setData({
+        _buttonsLayout: buttonsLayout,
+      });
+    }
   },
   methods: {
-    _onModalClick: function _onModalClick() {
-      const onModalClick = this.props.onModalClick;
+    _onModalClick() {
+      const { onModalClick } = this.props;
       if (onModalClick) {
         onModalClick();
       }
     },
-    _onButtonClick: function _onButtonClick(e) {
-      const onButtonClick = this.props.onButtonClick;
-      if (typeof onButtonClick === 'function') {
+    _onButtonClick(e) {
+      const { onButtonClick } = this.props;
+      if (onButtonClick) {
         onButtonClick(e);
       }
     },
-    _onModalClose: function _onModalClose() {
-      const onModalClose = this.props.onModalClose;
-      if (typeof onModalClose === 'function') {
+    _onModalClose() {
+      const { onModalClose } = this.props;
+      if (onModalClose) {
         onModalClose();
       }
     },
-    _onMaskTap: function _onMaskTap() {
-      const onMaskClick = this.props.onMaskClick;
+    _onMaskTap() {
+      const { onMaskClick } = this.props;
       if (typeof onMaskClick === 'function') {
         onMaskClick();
       } else {
