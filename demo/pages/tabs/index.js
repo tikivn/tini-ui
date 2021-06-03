4f59dbbcf64b16b4d12b080e956fbbadc541db72
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-06-03 14:40:47 +0700

    fix tabs

--- demo/pages/tabs/index.js
+++ demo/pages/tabs/index.js
@@ -4,15 +4,16 @@ Page({
       {
         title: 'Tab 1',
         subTitle: 'Subtitle 1',
-        number: '6',
-        image:
-          'https://cdn0.iconfinder.com/data/icons/google-material-design-3-0/48/ic_home_48px-256.png',
-          imageActive: 'https://cdn1.iconfinder.com/data/icons/real-estate-building-flat-vol-1/104/building__house__home__real__estate-512.png',
-        icon: 'home',
-        iconActive: 'success',
+        number: 'HOT',
+        // image:
+        //   'https://cdn0.iconfinder.com/data/icons/google-material-design-3-0/48/ic_home_48px-256.png',
+        // imageActive:
+        //   'https://cdn1.iconfinder.com/data/icons/real-estate-building-flat-vol-1/104/building__house__home__real__estate-512.png',
+        // icon: 'home',
+        // iconActive: 'success',
         showBadge: true,
         badge: {
-          arrow: true,
+          arrow: false,
           stroke: true,
         },
       },
@@ -26,20 +27,20 @@ Page({
           stroke: true,
         },
       },
-      {
-        title: 'Tab 3',
-        subTitle: 'Subtitle 3',
-        number: '99+',
-        showBadge: true,
-        badge: {
-          arrow: true,
-        },
-      },
-      { title: '4 Tab', subTitle: 'Subtitle 5', showBadge: true, number: 0 },
-      { title: '5 Tab', subTitle: 'Subtitle 5', number: '99+', showBadge: false },
-      { title: '3 Tab', subTitle: 'Subtitle 5', showBadge: false },
-      { title: '4 Tab', subTitle: 'Subtitle 5' },
-      { title: '15 Tab', subTitle: 'Subtitle 5' },
+      // {
+      //   title: 'Tab 3',
+      //   subTitle: 'Subtitle 3',
+      //   number: '99+',
+      //   showBadge: true,
+      //   badge: {
+      //     arrow: true,
+      //   },
+      // },
+      // { title: '4 Tab', subTitle: 'Subtitle 5', showBadge: true, number: 0 },
+      // { title: '5 Tab', subTitle: 'Subtitle 5', number: '99+', showBadge: false },
+      // { title: '3 Tab', subTitle: 'Subtitle 5', showBadge: false },
+      // { title: '4 Tab', subTitle: 'Subtitle 5' },
+      // { title: '15 Tab', subTitle: 'Subtitle 5' },
     ],
     activeTab: 0,
     isSwipeable: false,
--- src/badge/index.ts
+++ src/badge/index.ts
@@ -1,5 +1,6 @@
 Component({
   props: {
+    className: '',
     overflowCount: 99,
     text: '',
     dot: false,
