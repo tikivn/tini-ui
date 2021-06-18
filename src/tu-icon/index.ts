dbdab02926771d4af8aa4bb8eee60b4af0c9ce84
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-06-18 20:02:52 +0700

    add label

--- src/tu-icon/index.ts
+++ src/tu-icon/index.ts
@@ -3,11 +3,9 @@ import fmtUnit from '../_util/fmtUnit';
 Component({
   props: {
     type: '',
-    size: fmtUnit(17),
-    color: '',
+    size: fmtUnit('24px'),
+    color: '#27272A',
     className: '',
   },
-  data: {
-    valueUnit: fmtUnit('px'),
-  },
+  data: {},
 });
