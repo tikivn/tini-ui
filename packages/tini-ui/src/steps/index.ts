d6462f2658de7b06f5e5e438e07833bc95296c14
Lý Lưỡng Thiên <thien.ly@tiki.vn>
2021-09-08 09:55:50 +0700

    fix(tini-ui): Update steps as suggestion

--- packages/tini-ui/src/steps/index.ts
+++ packages/tini-ui/src/steps/index.ts
@@ -1,11 +1,15 @@
 Component({
   props: {
     className: '',
+    style: '',
     activeIndex: 0,
     failIndex: null,
     direction: 'horizontal',
     items: [],
-
+    inactiveColor: 'rgba(0, 0, 0, 0.1)',
+    activeColor: 'var(--brand, #1a94ff)',
+    dotErrorColor: '#ff424f',
+    dotSize: 'var(--space-inline-medium, 16px)',
   },
   data: {
   },
