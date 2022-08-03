59f88a273842789cc732aaab92481ba60ca13bf2
tri.nguyen1 <86943032+tri-tiki@users.noreply.github.com>
2022-08-03 20:42:41 +0700

    feat: new toast-bar (#59)

--- packages/tini-ui/src/toast-bar/index.ts
+++ packages/tini-ui/src/toast-bar/index.ts
@@ -1,22 +1,16 @@
 import fmtEvent from '../_util/fmtEvent';
 
 type ToastBarProps = {
-  type: 'success' | 'error' | 'info'; // success, error, info
+  type: 'neutral' | 'informative' | 'positive' | 'negative';
   iconLeft: string;
   closeIcon: string;
   show: boolean;
-  showIconLeft: boolean;
   showCloseIcon: boolean;
   content: string;
   actionText: string;
-  backgroundColor: string;
-  textColor: string;
   style: string;
   className: string;
-  textClass: string;
-  actionClass: string;
   closeButtonClass: string;
-  iconCClass: string;
   image: string;
   duration: number;
   placement: 'bottom' | 'top';
@@ -30,39 +24,27 @@ type ToastBarProps = {
 };
 
 type ToastBarData = {
-  icons: {
-    success: string;
-    error: string;
-    info: string;
-  };
-  toastBackground: string;
-  showToast: boolean;
+  internalShow: boolean;
 };
 
 // TODO: Handle multiple toast
 Component({
   props: {
-    type: 'success', // success, error, info
+    type: 'neutral',
     iconLeft: '',
     closeIcon: 'close',
-    show: false,
-    showIconLeft: true,
     showCloseIcon: true,
+    show: false,
     content: '',
     actionText: '',
-    backgroundColor: '', // Default is based on type
-    textColor: '#fff', // Apply for text, actionText, icon,...
     style: '',
     className: '',
-    textClass: '',
-    actionClass: '',
     closeButtonClass: '',
-    iconCClass: '',
     image: '', // To replace icon
     duration: 3000,
     placement: 'bottom', // bottom, top
-    top: '24px',
-    bottom: '24px',
+    top: null,
+    bottom: null,
     zIndex: 999,
     animation: true,
     animationDuration: 300,
@@ -70,64 +52,61 @@ Component({
     onClose: () => {},
   } as ToastBarProps,
   data: {
-    icons: {
-      success: 'success_glyph',
-      error: 'warning_glyph',
-      info: 'info_glyph',
-    },
-    toastBackground: '',
-    showToast: false,
+    internalShow: false,
   } as ToastBarData,
-  didMount() {
-    const { backgroundColor, type } = this.props;
 
-    if (backgroundColor) {
-      this.setData({ toastBackground: backgroundColor });
-    } else {
-      this.setData({
-        toastBackground:
-          {
-            success: 'var(--color-alias-positive)',
-            error: 'var(--color-alias-negative)',
-            info: 'var(--color-alias-reverse-theme)',
-          }[type] || 'var(--color-alias-positive)',
-      });
-    }
+  deriveDataFromProps(nextProps) {
+    this.checkShowToast(nextProps);
+  },
 
-    this.checkShowToast();
-  },
-  deriveDataFromProps() {
-    this.checkShowToast();
-  },
   didUnmount() {
     if (this.timeout) {
       clearTimeout(this.timeout);
     }
   },
+
   methods: {
     timeout: null,
     isShowing: false,
+
     onTapAction(event) {
       this.props.onTapAction(fmtEvent(this.props, event));
     },
+
     onTapClose(event) {
       const { onClose } = this.props;
+
       clearTimeout(this.timeout);
-      this.setData({ showToast: false }, () => onClose(fmtEvent(this.props, event)));
+
+      this.setData(
+        {
+          internalShow: false,
+        },
+        () => onClose(fmtEvent(this.props, event)),
+      );
     },
-    checkShowToast() {
-      const { show, duration, onClose } = this.props;
-      const { showToast } = this.data;
-      if (!show || showToast) {
-        return;
-      }
-      this.setData({ showToast: true });
-      if (this.timeout) {
-        clearTimeout(this.timeout);
-      }
-      this.timeout = setTimeout(() => {
-        this.setData({ showToast: false }, () => onClose(fmtEvent(this.props)));
-      }, duration);
+
+    checkShowToast(nextProps) {
+      const { show, duration, onClose } = nextProps;
+      const { internalShow } = this.data;
+
+      if (show === internalShow) return;
+
+      this.setData({ internalShow: show }, () => {
+        if (this.timeout) {
+          clearTimeout(this.timeout);
+        }
+
+        if (show)
+          this.timeout = setTimeout(() => {
+            this.setData(
+              {
+                internalShow: false,
+              },
+              () => onClose(fmtEvent(this.props)),
+            );
+          }, duration);
+      });
     },
   },
 });
